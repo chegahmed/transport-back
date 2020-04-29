6dfeb5b7851841a48a6cfcf234ab86be51d2dab0
achega <chegahemed@gmail.com>
2020-04-29 18:35:04 +0200

    first commit

---
+++ src/main/java/com/transport/TransportBackApplication.java
@@ -0,0 +1,14 @@
+package com.transport;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportBackApplication  implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportBackApplication.class, args);
+	}
+
+}
